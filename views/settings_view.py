96aef458bf8b36e30de7c105b1895c22bd078422
noidoRG <noidorga@gmail.com>
2024-06-12 20:51:20 +0300

    Унифицирована работа с модулем настроек

--- views/settings_view.py
+++ views/settings_view.py
@@ -2,8 +2,14 @@
 
 from PyQt6.QtWidgets import QWidget, QVBoxLayout, QPushButton, QMessageBox
 from controllers.settings_controller import SettingsController
+from PyQt6.QtCore import QObject, pyqtSignal
+import json
+import glob
 
 class SettingsView(QWidget):
+    learnt_reset = pyqtSignal()
+    mastered_reset = pyqtSignal()
+
     def __init__(self):
         super().__init__()
 
@@ -32,10 +38,35 @@ class SettingsView(QWidget):
         if reply == QMessageBox.StandardButton.Yes:
             self.reset_mastered_gestures()
 
+    # def reset_learnt_gestures(self):
+    #     self.controller.reset_learnt_gestures()
+    #     QMessageBox.information(self, "Сброс данных", "Данные об изученных жестах сброшены.")
+
+    # def reset_mastered_gestures(self):
+    #     self.controller.reset_mastered_gestures()
+    #     QMessageBox.information(self, "Сброс данных", "Данные об освоенных жестах сброшены.")
+
+# Работа с JSON
     def reset_learnt_gestures(self):
-        self.controller.reset_learnt_gestures()
+        for filename in glob.glob("data/*.json"):
+            with open(filename, 'r+', encoding='utf-8') as f:
+                data = json.load(f)
+                data['learnt'] = False
+                data['mastered'] = False
+                f.seek(0)
+                json.dump(data, f, ensure_ascii=False, indent=4)
+                f.truncate()
+        self.learnt_reset.emit()
         QMessageBox.information(self, "Сброс данных", "Данные об изученных жестах сброшены.")
 
+
     def reset_mastered_gestures(self):
-        self.controller.reset_mastered_gestures()
+        for filename in glob.glob("data/*.json"):
+            with open(filename, 'r+', encoding='utf-8') as f:
+                data = json.load(f)
+                data['mastered'] = False
+                f.seek(0)
+                json.dump(data, f, ensure_ascii=False, indent=4)
+                f.truncate()
+        self.mastered_reset.emit()
         QMessageBox.information(self, "Сброс данных", "Данные об освоенных жестах сброшены.")
